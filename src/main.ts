5832233058815c914be6264be8589baee1aa9d13
Jean Lima <jeansantoslima17@gmail.com>
2025-05-14 18:54:27 -0300

    chore(dependencies): add class-validator, class-transformer and @nestjs/swagger for API docs.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,8 +9,18 @@ async function bootstrap() {
   app.enableCors();
 
   const logger = new Logger();
-  logger.log(`Server running on http://localhost:${process.env.PORT ?? 3000}`);
-  
+  logger.log(`Server running on http://localhost:${process.env.PORT ?? 3000}`, 'Bootstrap');
+  logger.log(`Documentation running on http://localhost:${process.env.PORT ?? 3000}/api-docs`, 'Bootstrap');
+
+  const config = new DocumentBuilder()
+    .setTitle('Elos API')
+    .setDescription('Documentação da API para o front-end do sistema ELOS')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       stopAtFirstError: true,
