d154d568fdfb320e71305b306b290b67df98c829
Jean Lima <jeansantoslima17@gmail.com>
2025-05-19 12:01:40 -0300

    feat: update AppController to respond to '/ping' endpoint and configure AppModule with necessary imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ComplaintsModule } from './modules/complaints/complaints.module';
+import { SupportRequestsModule } from './modules/support-requests/support-requests.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    AuthModule,
+    ComplaintsModule,
+    SupportRequestsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
